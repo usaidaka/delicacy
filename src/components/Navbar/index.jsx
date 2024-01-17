dbcea36b04c663b609b79965e396c31d59065fdc
usaid AKA <ujedkemal@gmail.com>
2024-01-17 17:09:10 +0700

    styling and some fetching

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,61 +1,65 @@
 import * as React from "react";
-import Tabs, { tabsClasses } from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import classes from "./style.module.scss";
+import { Link, useSearchParams } from "react-router-dom";
+import { Button } from "@mui/material";
 
-export default function Navbar({ setMyMode }) {
-  const [value, setValue] = React.useState("one");
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
+export default function Navbar({ setMyMode, categories, setter, action }) {
   return (
     <>
       {/* Desktop */}
-      <Box sx={{ width: "100%" }} className={classes["tab-desktop"]}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="primary"
-          indicatorColor="primary"
-          aria-label="primary tabs example"
+
+      <Box className={classes["tab-desktop"]}>
+        {categories.map((category) => (
+          <Button
+            key={category.idCategory}
+            value={category.idCategory}
+            label={category.strCategory}
+            color="inherit"
+            sx={{ color: "#969696" }}
+            onClick={() => {
+              setter(category.strCategory);
+              action();
+            }}
+          >
+            {category.strCategory}
+          </Button>
+        ))}
+        <Button
+          value={"favorite"}
+          label={"favorite"}
+          color="inherit"
+          sx={{ color: "#969696" }}
         >
-          <Tab value="one" label="Item One" />
-          <Tab value="two" label="Item Two" />
-          <Tab value="three" label="Item Three" />
-        </Tabs>
+          <Link to="/favorite">Favorite</Link>
+        </Button>
       </Box>
       {/* Mobile */}
-      <Box
-        sx={{
-          flexGrow: 1,
-          maxWidth: { xs: 320, sm: 480 },
-          bgcolor: "background.paper",
-        }}
-        className={classes["tab-mobile"]}
-      >
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="scrollable"
-          scrollButtons
-          aria-label="visible arrows tabs example"
-          sx={{
-            [`& .${tabsClasses.scrollButtons}`]: {
-              "&.Mui-disabled": { opacity: 0.3 },
-            },
-          }}
+
+      <Box className={classes["tab-mobile"]}>
+        {categories.map((category) => (
+          <Button
+            key={category.idCategory}
+            value={category.idCategory}
+            label={category.strCategory}
+            color="inherit"
+            sx={{ color: "#969696" }}
+            onClick={(e) => {
+              setter(category.strCategory);
+              action();
+            }}
+          >
+            {category.strCategory}
+          </Button>
+        ))}
+        <Button
+          value={"favorite"}
+          label={"favorite"}
+          color="inherit"
+          sx={{ color: "#969696" }}
         >
-          <Tab value={"one"} label="Item One" />
-          <Tab value={"two"} label="Item Two" />
-          <Tab value={"three"} label="Item Three" />
-          <Tab value={"four"} label="Item Four" />
-          <Tab value={"five"} label="Item Five" />
-          <Tab value={"six"} label="Item Six" />
-          <Tab value={"seven"} label="Item Seven" />
-        </Tabs>
+          <Link to="/favorite">Favorite</Link>
+        </Button>
       </Box>
     </>
   );
--- src/pages/ListByCategory/index.jsx
+++ src/pages/ListByCategory/index.jsx
@@ -5,30 +5,46 @@ import CardDetail from "../../components/CardDetail";
 import { Box } from "@mui/material";
 import classes from "./style.module.scss";
 import CardRecipes from "../../components/CardRecipes";
+import { useSearchParams } from "react-router-dom";
 
 const ListByCategory = () => {
   const [categories, setCategories] = useState([]);
+  const [valueCategory, setValueCategory] = React.useState("Beef");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const selectedCategory = searchParams.get("category");
 
   useEffect(() => {
+    setSearchParams({ category: valueCategory });
     fetchCategories();
-  }, []);
+  }, [valueCategory]);
 
   const fetchCategories = async () => {
     try {
-      const headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept",
-      };
+      const response = await callApi("/categories.php", "GET");
+      setCategories(response.categories.slice(0, 6));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      const response = await callApi("/categories.php", "GET", headers);
-      console.log(response);
-
-      // const x = await fetch(
-      //   "https://www.themealdb.com/api/json/v1/1/categories.php"
-      // ).then((res) => res.json());
-
-      // console.log(x);
+  const fetchProduct = async () => {
+    try {
+      const responseByCategory = await callApi(
+        `/filter.php?c=${selectedCategory}`,
+        "GET"
+      );
+      const sliceResponse = responseByCategory?.meals?.slice(0, 10);
+      const modifiedResponse = sliceResponse?.map(async (item) => {
+        const responseByName = await callApi(
+          `/search.php?s=${item.strMeal}`,
+          "GET"
+        );
+        const { idMeal } = responseByName.meals[0];
+        return idMeal;
+      });
+      const finalResponse = await Promise.all(modifiedResponse);
+      console.log(finalResponse, "testsss");
     } catch (error) {
       console.log(error);
     }
@@ -36,9 +52,12 @@ const ListByCategory = () => {
 
   return (
     <>
-      <Navbar />
+      <Navbar
+        categories={categories}
+        setter={setValueCategory}
+        action={fetchProduct}
+      />
       <Box className={classes["card-detail-container"]}>
-        <CardDetail />
         <CardDetail />
         <CardDetail />
         <CardDetail />
